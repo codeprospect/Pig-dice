ce6e288a77e25a94b0edc975f2965f7f322223a8
waikau maina <mainaeric2099@gmail.com>
2019-03-01 12:16:53 +0300

    assigned the results to a variable

--- js/scripts.js
+++ js/scripts.js
@@ -16,30 +16,40 @@ $(document).ready(function() {
 
 //this is the one pig section
     $("#roll1").click(function() {
-      document.getElementById('dice1').innerHTML = Math.floor(Math.random() * 6 + 1);
+      var result = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('dice1').innerHTML =  result;
     });
     $("#roll2").click(function() {
-      document.getElementById('dice2').innerHTML = Math.floor(Math.random() * 6 + 1);
+      var result2 = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('dice2').innerHTML = result2;
     });
 
 //this is thr two dice pig section
     $("#rolla1").click(function() {
-      document.getElementById('dicea1').innerHTML = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('dicea2').innerHTML = Math.floor(Math.random() * 6 + 1);
+      var dicea1 = Math.floor(Math.random() * 6 + 1);
+      var dicea2 = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('dicea1').innerHTML = dicea1;
+      document.getElementById('dicea2').innerHTML = dicea2;
     });
     $("#rolla2").click(function() {
-      document.getElementById('diceb1').innerHTML = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('diceb2').innerHTML = Math.floor(Math.random() * 6 + 1);
+      var diceb1 = Math.floor(Math.random() * 6 + 1);
+      var diceb2 = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('diceb1').innerHTML = diceb1;
+      document.getElementById('diceb2').innerHTML = diceb2;
     });
 
 //this is the big pig section
     $("#rollx1").click(function() {
-      document.getElementById('diceo1').innerHTML = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('diceo2').innerHTML = Math.floor(Math.random() * 6 + 1);
+      var diceo1 = Math.floor(Math.random() * 6 + 1);
+      var diceo2 = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('diceo1').innerHTML = diceo1;
+      document.getElementById('diceo2').innerHTML = diceo2;
     });
     $("#rollx2").click(function() {
-      document.getElementById('dicep1').innerHTML = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('dicep2').innerHTML = Math.floor(Math.random() * 6 + 1);
+      var dicep1 = Math.floor(Math.random() * 6 + 1);
+      var dicep2 =  Math.floor(Math.random() * 6 + 1);
+      document.getElementById('dicep1').innerHTML = dicep1;
+      document.getElementById('dicep2').innerHTML = dicep2;
     });
 
 });
