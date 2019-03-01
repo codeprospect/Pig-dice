37ea1a130347533d5598cf25fb7c92743e9901fd
waikau maina <mainaeric2099@gmail.com>
2019-03-01 11:46:13 +0300

    put rules in cards and styled them

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,42 @@
 $(document).ready(function() {
+  $("#onedii").click(function() {
+    $(".onedice").show();
+    $("#rules").hide();
+  });
+
+  $("#twodii").click(function() {
+    $(".twodice").show();
+    $("#rules").hide();
+  });
+
+  $("#bigpig").click(function() {
+    $(".pibig").show();
+    $("#rules").hide();
+  });
+
     $("#roll1").click(function() {
       document.getElementById('dice1').innerHTML = Math.floor(Math.random() * 6 + 1);
     });
     $("#roll2").click(function() {
       document.getElementById('dice2').innerHTML = Math.floor(Math.random() * 6 + 1);
     });
+
+    $("#rolla1").click(function() {
+      document.getElementById('dicea1').innerHTML = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('dicea2').innerHTML = Math.floor(Math.random() * 6 + 1);
+    });
+    $("#rolla2").click(function() {
+      document.getElementById('diceb1').innerHTML = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('diceb2').innerHTML = Math.floor(Math.random() * 6 + 1);
+    });
+
+    $("#rollx1").click(function() {
+      document.getElementById('diceo1').innerHTML = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('diceo2').innerHTML = Math.floor(Math.random() * 6 + 1);
+    });
+    $("#rollx2").click(function() {
+      document.getElementById('dicep1').innerHTML = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('dicep2').innerHTML = Math.floor(Math.random() * 6 + 1);
+    });
+
 });
