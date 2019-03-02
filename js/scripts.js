6c223a468fd59008a74f174357b8cb3e0718996d
waikau maina <mainaeric2099@gmail.com>
2019-03-02 18:21:13 +0300

    linked the game page to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,11 @@ $(document).ready(function() {
     $("#rules").hide();
   });
 
-  $("#twodii").click(function() {
-    $(".twodice").show();
-    $("#rules").hide();
-  });
-
-  $("#bigpig").click(function() {
-    $(".pibig").show();
-    $("#rules").hide();
-  });
-
 var score = 0;
 //this is the one pig section
     $("#roll1").click(function() {
       var result = Math.floor(Math.random() * 6 + 1);
       document.getElementById('dice1').innerHTML =  result;
-      $('#scores').append(result);
       var playerones = [];
 
       if (result == 1) {
@@ -31,7 +20,6 @@ var score = 0;
           playerones.forEach(function(playerone) {
              score += playerone;
           });
-          alert(score);
         });
     });
     $("#roll2").click(function() {
@@ -40,33 +28,33 @@ var score = 0;
     });
 
 //this is thr two dice pig section
-    $("#rolla1").click(function() {
-      var dicea1 = Math.floor(Math.random() * 6 + 1);
-      var dicea2 = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('dicea1').innerHTML = dicea1;
-      document.getElementById('dicea2').innerHTML = dicea2;
-      $("#score").append(dicea1 + dicea2);
-    });
-    $("#rolla2").click(function() {
-      var diceb1 = Math.floor(Math.random() * 6 + 1);
-      var diceb2 = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('diceb1').innerHTML = diceb1;
-      document.getElementById('diceb2').innerHTML = diceb2;
-      $("#score2").append(diceb1 + diceb2);
-    });
+    // $("#rolla1").click(function() {
+    //   var dicea1 = Math.floor(Math.random() * 6 + 1);
+    //   var dicea2 = Math.floor(Math.random() * 6 + 1);
+    //   document.getElementById('dicea1').innerHTML = dicea1;
+    //   document.getElementById('dicea2').innerHTML = dicea2;
+    //   $("#score").append(dicea1 + dicea2);
+    // });
+    // $("#rolla2").click(function() {
+    //   var diceb1 = Math.floor(Math.random() * 6 + 1);
+    //   var diceb2 = Math.floor(Math.random() * 6 + 1);
+    //   document.getElementById('diceb1').innerHTML = diceb1;
+    //   document.getElementById('diceb2').innerHTML = diceb2;
+    //   $("#score2").append(diceb1 + diceb2);
+    // });
 
 //this is the big pig section
-    $("#rollx1").click(function() {
-      var diceo1 = Math.floor(Math.random() * 6 + 1);
-      var diceo2 = Math.floor(Math.random() * 6 + 1);
-      document.getElementById('diceo1').innerHTML = diceo1;
-      document.getElementById('diceo2').innerHTML = diceo2;
-    });
-    $("#rollx2").click(function() {
-      var dicep1 = Math.floor(Math.random() * 6 + 1);
-      var dicep2 =  Math.floor(Math.random() * 6 + 1);
-      document.getElementById('dicep1').innerHTML = dicep1;
-      document.getElementById('dicep2').innerHTML = dicep2;
-    });
+    // $("#rollx1").click(function() {
+    //   var diceo1 = Math.floor(Math.random() * 6 + 1);
+    //   var diceo2 = Math.floor(Math.random() * 6 + 1);
+    //   document.getElementById('diceo1').innerHTML = diceo1;
+    //   document.getElementById('diceo2').innerHTML = diceo2;
+    // });
+    // $("#rollx2").click(function() {
+    //   var dicep1 = Math.floor(Math.random() * 6 + 1);
+    //   var dicep2 =  Math.floor(Math.random() * 6 + 1);
+    //   document.getElementById('dicep1').innerHTML = dicep1;
+    //   document.getElementById('dicep2').innerHTML = dicep2;
+    // });
 
 });
